0362d1d3a40313ffe5cf299d733ca54c5c2efe4a
= <=>
2021-10-11 21:26:19 +0900

    프로그래머스 해시2 전화번호부 문제

--- src/prg/prg_1011_1.java
+++ src/prg/prg_1011_1.java
@@ -1,12 +1,11 @@
 package prg;
 
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 public class prg_1011_1 {
 
     public static void main(String[] args) {
-        String[] strArr = {"119", "97674223", "1195524421"};
+        String[] strArr = {"12", "112","113", "122"};
         System.out.println(solution(strArr));
 
     }
@@ -14,16 +13,15 @@ public class prg_1011_1 {
     public static boolean solution(String[] phone_book){
         boolean answer = true;
         int length = phone_book.length;
-        Arrays.sort(phone_book, Comparator.comparing(String::length));
 
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                if (phone_book[j].startsWith(phone_book[i])) {
-                    return false;
-                }
+        Arrays.sort(phone_book);
+        //System.out.println(Arrays.toString(phone_book));
+
+        for (int i = 0; i < length-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                return false;
             }
         }
-
         return answer;
     }
 }

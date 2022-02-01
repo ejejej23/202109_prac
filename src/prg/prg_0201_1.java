c04bfa5389c584d2edcb36e926327ce3b4b0255e
Yang.Eunjin <nador1123@naver.com>
2022-02-01 13:36:37 +0900

    더맵게 문제

---
+++ src/prg/prg_0201_1.java
@@ -0,0 +1,40 @@
+package prg;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class prg_0201_1 {
+	public static void main(String[] args) {
+		int[] scoville = {12, 1, 2, 3, 8, 10};
+		System.out.println(Solution.solution(scoville, 7));
+	}
+
+	static class Solution {
+		public static int solution(int[] scoville, int K) {
+			int answer = 0;
+
+			boolean isOverK = Arrays.stream(scoville).allMatch(a -> (int) a >= K);
+			if(isOverK) return 0;
+
+			List<Integer> arr1 = Arrays.stream(scoville).sorted().limit(2).boxed().collect(Collectors.toList());
+			List<Integer> arr2 = Arrays.stream(scoville).sorted().skip(2).boxed().collect(Collectors.toList());
+
+			for (int i = 0; !isOverK; i++) {
+				int mix = arr1.get(0) + (arr1.get(1) * 2);
+				arr2.add(mix);
+				answer++;
+				isOverK = arr2.stream().allMatch(b-> b>=K);
+
+				if(arr2.stream().count()<2){
+                    if(isOverK) return answer;
+                    else return -1;
+                }
+
+				arr1 = arr2.stream().sorted().limit(2).collect(Collectors.toList());
+				arr2 = arr2.stream().sorted().skip(2).collect(Collectors.toList());
+			}
+			return answer;
+		}
+	}
+}

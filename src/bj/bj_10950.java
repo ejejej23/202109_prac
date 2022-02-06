a22a21eb00f3200071c1e42d87ce190d5c9925d1
Yang.Eunjin <nador1123@naver.com>
2022-02-06 18:02:37 +0900

    나머지문제

---
+++ src/bj/bj_10950.java
@@ -0,0 +1,15 @@
+package bj;
+
+import java.util.Scanner;
+
+public class bj_10950 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while (t-- >0){
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+		}
+	}
+}

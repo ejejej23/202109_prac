7baad5f357cd0603d2770bef3b4f3ddfb987195d
Yang.Eunjin <nador1123@naver.com>
2022-02-27 00:08:24 +0900

    복습1

---
+++ src/bj/bj_1978.java
@@ -0,0 +1,30 @@
+package bj;
+
+import java.util.*;
+public class bj_1978 {
+    //소수 구하기
+    public static boolean is_prime(int x) {
+        if (x <= 1) {
+            return false;
+        } else if (x == 2) {
+            return true;
+        }
+        for (int i=2; i*i <= x; i++) {
+            if (x % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ans = 0;
+        while (n-- > 0) {
+            if (is_prime(sc.nextInt())) {
+                ans += 1;
+            }
+        }
+        System.out.println(ans);
+    }
+}

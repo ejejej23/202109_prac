9e5f649703b936acbdaa23c405e5a79fbe41abe9
Yang.Eunjin <nador1123@naver.com>
2022-03-08 21:07:00 +0900

    복습8

---
+++ src/bj/bj_11055.java
@@ -0,0 +1,31 @@
+package bj;
+
+import java.util.*;
+
+public class bj_11055 {
+	//가장 큰 증가하는 부분 수열
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        for (int i=0; i<n; i++) {
+            a[i] = sc.nextInt();
+        }
+        int[] d = new int[n];
+        for (int i=0; i<n; i++) {
+            d[i] = a[i];
+            for (int j=0; j<i; j++) {
+                if (a[j] < a[i] && d[i] < d[j]+a[i]) {
+                    d[i] = d[j]+a[i];
+                }
+            }
+        }
+        int ans = d[0];
+        for (int i=0; i<n; i++) {
+            if (ans < d[i]) {
+                ans = d[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
